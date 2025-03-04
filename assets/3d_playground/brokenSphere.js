00e27b2f3aed41dc5859d5fb6818470a27ac31ad
DDstar1 <73956903+DDstar1@users.noreply.github.com>
2025-03-04 22:56:27 +0100

    Update brokenSphere.js

--- assets/3d_playground/brokenSphere.js
+++ assets/3d_playground/brokenSphere.js
@@ -20,7 +20,7 @@ export function createSplitSphere({
   scale = 1,
   maxSeparation = 2,
   numSections = 70,
-  pauseDuration = 3000, // 4 seconds
+  pauseDuration = 2000, // 4 seconds
   scatterAxis = "xyz", // Options: "x", "y", "z", "xy", "xz", "yz", "xyz"
   color_list = default_color_list,
 }) {
