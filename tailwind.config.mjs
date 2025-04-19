e201c53652a8d21214d960d08802961210eb5b69
DDstar1 <abhuluimendestiny@gmail.com>
2025-04-19 01:36:56 +0100

    'svsdvd'

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,11 +8,11 @@ export default {
   ],
   theme: {
     screens: {
-      "2xl": { max: "1535px" }, // Styles apply below 1535px
-      xl: { max: "1279px" }, // Styles apply below 1279px
-      lg: { max: "1023px" }, // Styles apply below 1023px
-      md: { max: "767px" }, // Styles apply below 767px
-      sm: { max: "639px" }, // Styles apply below 639px
+      "2xl": "1536px",
+      xl: "1280px",
+      lg: "1024px",
+      md: "900px",
+      sm: "600px",
     },
     extend: {
       colors: {
